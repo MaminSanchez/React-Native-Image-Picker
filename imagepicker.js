77110dc3213e904c07860e01f05e90d7ce48b4f5
MaminSanchez <138103167+MaminSanchez@users.noreply.github.com>
2023-06-29 10:38:57 +0300

    Create imagepicker.js

---
+++ imagepicker.js
@@ -0,0 +1,7 @@
+// Implement an image picker feature in a React Native app
+
+import React, { useState } from 'react';
+import { View, Button, Image } from 'react-native';
+import ImagePicker from 'react-native-image-picker';
+
+const App
